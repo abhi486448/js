72a4ffaf40e03ef0a64c4389daa651df10b52a71
Abhishek Gupta <146754692+abhi486448@users.noreply.github.com>
2024-12-11 17:07:04 +0000

    Add 02_Datatype

---
+++ 01_basics/02_DataType.js
@@ -0,0 +1,26 @@
+"use strict"; // treate all js code as newer version
+
+// alert( 3 + 3) // we are using node.js, not browser
+
+console.log(3 
+    +
+     3);  // code readabilty should be high
+
+console.log("Abhishek")
+
+let name = "Abhishek"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
